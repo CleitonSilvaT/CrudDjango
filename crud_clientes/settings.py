7bb00a96e417cb2dc67c657c65b026167a6d096a
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-09-04 06:27:47 -0300

    Criado template. Close #4

--- crud_clientes/settings.py
+++ crud_clientes/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_DIR = Path.joinpath(BASE_DIR, 'static')
+TEMPLATE_DIR = Path.joinpath(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -56,7 +57,7 @@ ROOT_URLCONF = 'crud_clientes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
