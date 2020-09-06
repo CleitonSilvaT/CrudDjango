f1bb935d1c1c53276ef43da2145ea66841f2d9c4
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-09-06 13:39:23 -0300

    View Listar Clientes Criada. Close #5

--- clientes/models.py
+++ clientes/models.py
@@ -3,9 +3,9 @@ from django.db import models
 class Cliente(models.Model):
     nome = models.CharField(max_length=50)
     email = models.EmailField()
-    cpf = models.CharField(max_length=11, unique=True) # Tamanho contando os pontos
+    cpf = models.CharField(max_length=14, unique=True) # Tamanho contando os pontos
     data_criacao = models.DateField(auto_now_add=True)
 
     def __str__(self): # Definir o que será mostrado no Admin
-        return self.name
+        return self.nome
     
--- clientes/views.py
+++ clientes/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Cliente
 
 # Create your views here.
+
+def listar_clientes(request):
+    clientes = Cliente.objects.all().order_by('-id')
+
+    
+    return render(request, 'clientes/listar_clientes.html', {'clientes':clientes})
