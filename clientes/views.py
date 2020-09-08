889ded58ceeb0a765c734d2a7ad9fff7ab900ba3
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-09-08 06:12:34 -0300

    Campo de pesquisa criado. Close #7

--- clientes/views.py
+++ clientes/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from django.db.models import Q
 from .models import Cliente
 
 # Create your views here.
 
 def listar_clientes(request):
     clientes = Cliente.objects.all().order_by('-id')
+    queryset = request.GET.get('q')
+
+    if queryset:
+        clientes = Cliente.objects.filter(
+            Q(nome__icontains = queryset)|
+            Q(cpf__icontains = queryset)|
+            Q(email__icontains = queryset)
+        )
 
     paginator = Paginator(clientes, 5)
     page = request.GET.get('page')
