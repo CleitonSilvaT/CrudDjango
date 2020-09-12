eea6040b62cc16010ba0bd7be74d7bf9694dbd38
CleitonSilvaT <cleitonsilvatavares@gmail.com>
2020-09-12 06:22:14 -0300

    Criada integração com Heroku. Close #14

--- crud_clientes/settings.py
+++ crud_clientes/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'lvdm6^-1)y8c%84gi)5ks+4ww48fuwm)czwnywsrn3(w8^#zso'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['crud-clientes-django.heroku.app']
 
 
 # Application definition
